ddfe518d550ab4edb8e71a2c6fa8efcd40fc412b
meemansaaa <155892997+meemansaaa@users.noreply.github.com>
2025-09-05 12:52:14 +0530

    triplicates.py

--- triplicates.py
+++ triplicates.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
 
-file = 'dnawise_3.xlsx'
+file = 'filename_3.xlsx'
 df = pd.read_excel(file)
 print(df.head(5))
 
 def triplicate():
     newdf = pd.DataFrame(np.repeat(df.values, 3, axis = 0))
     newdf.columns = df.columns #to copy the same column names
-    newdf.to_excel('triplicated_dnawise3.xlsx')
+    newdf.to_excel('triplicated_filename.xlsx')
     print(newdf.head(6))
+
 
 
 triplicate()
